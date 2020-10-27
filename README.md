727d8844075782905d5cd56da29634e0f6b7f924
Urs Schaufelberger <urs.schaufelberger@protonmail.com>
2020-10-27 09:20:57 +0100

    add sketch & anigif

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# mz2020_distance
-Make Faire 2020 APDS9960 proximity sensor demo
+#Make Faire 2020 APDS9960 proximity sensor demo.
+
+![Proximity sensor demo](mz2020_proximity.gif)
+
+Features:
+- Periodically read the APDS9960 proximity sensor
+- Control the MK2020 badge lights accordingly
+
+Required Arduino libraries:
+- Adafruit_Neopixel
+- Arduino_APDS9960
---
+++ mz2020_distance.ino
@@ -0,0 +1,40 @@
+#include <Adafruit_NeoPixel.h>
+#include <Arduino_APDS9960.h>
+
+// Make Zurich 2020 badge LED configuration
+#define LED_PIN 4
+#define LED_COUNT 11
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+// This routine runs once upon power on
+void setup() {
+  // Initialize serial monitor for debugging
+  Serial.begin(9600);
+
+  // Initialize ADPS9660 sensor
+  if (!APDS.begin()) {
+    Serial.println("Error initializing APDS9960 sensor!");
+  }
+  
+  // Inizialize Neopixel LED strip
+  strip.begin();
+  strip.setBrightness(255);
+  strip.show(); // Initialize all pixels to 'off'
+
+}
+
+// Global variable to keep track of proximity reading
+int proximity = 0;
+
+// This runs repeatedly, forever
+void loop() {
+  if (APDS.proximityAvailable()) {
+    proximity = APDS.readProximity();
+  }
+  int brightness = 255 - proximity;
+  for(int i=0; i<LED_COUNT; i++) { // For each pixel...
+    strip.setPixelColor(i, strip.Color(brightness, brightness, brightness));
+  }
+  strip.show();   // Send the updated pixel colors to the hardware.
+}
